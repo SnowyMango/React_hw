442d417f2e2b535b4382b7f93e2cdb17e7350542
Deni Xhafa <denixhafa2@gmail.com>
2026-04-30 15:41:37 +0200

    no idea

--- app/(tabs)/sdfzgxn.tsx
+++ app/(tabs)/sdfzgxn.tsx
@@ -1,16 +1,13 @@
-import { usePathname, useRouter } from 'expo-router';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function Home() {
-  const router = useRouter();
-  const pathname = usePathname();
 
   return (
     <View style={styles.container}>
       <Text> bjorn meksi</Text>
     </View>
   )
-
+}
   const styles = StyleSheet.create({
     container: {
       flex: 1,
